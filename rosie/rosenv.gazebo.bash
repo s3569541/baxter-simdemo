e724a6686e37b44b782f31df6d2c1e7e1a759e0b
Ian Peake <ian.peake@rmit.edu.au>
2020-08-12 13:43:12 +1000

    commented-out capability to use dynamic link connector

--- rosie/rosenv.gazebo.bash
+++ rosie/rosenv.gazebo.bash
@@ -2,7 +2,8 @@
 
 source ~/rosie/rosenv.common.bash
 
-# These three are all implicitly imported by mir_ws/devel/setup.bash
+# This chain are all implicitly imported by the successor
 #source ~/ws_baxter/devel/setup.bash
 #source ~/mb_ws/devel/setup.bash
 source ~/mir_ws/devel/setup.bash
+#source ~/gazebo_link_attacher_ws/devel/setup.bash
