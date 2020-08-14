ec722ee5f8024d71a69b66fca8a7579299a70140
Ian Peake <ian.peake@rmit.edu.au>
2020-08-14 12:29:19 +1000

    improve pickup code to increase reliability

--- rosie/ikeg/block_pickup_test.py
+++ rosie/ikeg/block_pickup_test.py
@@ -458,8 +458,9 @@ gripper.set_holding_force(100)
 gripper.close()
 gripper.open()
 
-solve_move_trac(mylimb, make_pose_stamped(Vectors.V4D(0.49, -0.0, 0.0, 0), frame_id='base'))
-solve_move_trac(mylimb, make_pose_stamped(Vectors.V4D(0.49, -0.01, -0.16, 0), frame_id='base'))
+solve_move_trac(mylimb, make_pose_stamped(Vectors.V4D(0.49, 0.0, 0.0, 0), frame_id='base'))
+solve_move_trac(mylimb, make_pose_stamped(Vectors.V4D(0.49, 0.0, -0.1, 0), frame_id='base'))
+solve_move_trac(mylimb, make_pose_stamped(Vectors.V4D(0.49, 0.0, -0.16, 0), frame_id='base'))
 
 rospy.sleep(1.0)
 gripper.close()
@@ -467,7 +468,7 @@ gripper.open()
 gripper.close()
 rospy.sleep(2.0)
 
-solve_move_trac(mylimb, make_pose_stamped(Vectors.V4D(0.49, -0.0, 0.0, 0), frame_id='base'))
+solve_move_trac(mylimb, make_pose_stamped(Vectors.V4D(0.49, 0.0, 0.0, 0), frame_id='base'))
 
 #while True:
 #    rospy.sleep(1.0)
