6f56e4286f8d4f2b201c0d5b9146bb31831402f3
Ian Peake <ian.peake@rmit.edu.au>
2020-08-28 11:16:14 +1000

    correctly define target marker; correctly define target pose frame for right arm; avgpos is computed slightly differently

--- rosie/ikeg/pickup_alvar_demo.py
+++ rosie/ikeg/pickup_alvar_demo.py
@@ -53,7 +53,8 @@ from std_msgs.msg import Header
 #mutex = threading.Lock()
 
 def this_marker(marker):
-  return frame != 'head_camera' and block_nr(marker) == 1
+    result = (marker.header.frame_id != 'head_camera') and (locallib.block_nr(marker) == 1)
+    return result
 
 locallib.init(nodename='pickup_alvar_demo',target_marker_fn=this_marker)
 
@@ -108,10 +109,11 @@ print 'true RPW',trueroll,truepitch,trueyaw
 
 global mylimb
 mylimb = 'left'
+global otherlimb
+otherlimb = 'right'
 
-print 'calibrate / open gripper'
+print 'init / open gripper'
 gripper = locallib.init_gripper(mylimb)
-gripper.calibrate()
 gripper.open()
 rospy.sleep(1)
 print 'gripper open'
@@ -124,30 +126,42 @@ avgyaw = None
 def getavgpos():
     global avgpos
     global avgyaw
-    (avgpos,avgyaw) = locallib.getavgpos(target_marker_id)
-    print 'demo:','avgyaw',avgyaw
+    print 'awaiting marker update'
+    rospy.sleep(2)
+    result = locallib.getavgpos(target_marker_id)
+    if result != None:
+        (avgpos,avgyaw) = locallib.getavgpos(target_marker_id)
+        print 'marker update:',(avgpos,avgyaw)
 
+# FIXME: back off and try from more positions
+# FIXME: point right camera at best guess position
+# FIXME: give up if the marker is consistently not found
+
+otherpose = locallib.get_frame(otherlimb+'_gripper','base')
+print 'camera on',otherlimb,'...'
+locallib.solve_move_trac(otherlimb, locallib.make_pose_stamped_rpy(otherpose.transform.translation, frame_id='base', r=3.14, p=0.01))
+
+avgpos = None
 print '******* move to basic sighting pos ********'
 print 'look for cube from pos1'
 locallib.solve_move_trac(mylimb, locallib.make_pose_stamped(Point(x=0.49, y=-0.02, z=0.0), frame_id='base'))
+getavgpos()
 print 'look for cube from pos2'
 locallib.solve_move_trac(mylimb, locallib.make_pose_stamped(Point(x=0.49, y= 0.02, z=0.0), frame_id='base'))
+getavgpos()
 
 # should have a sighting by now...?
-while avgpos == None:
-    rospy.sleep(2)
-    getavgpos()
 
 print '******* move to proper sighting pos ********'
-locallib.solve_move_trac(mylimb, locallib.make_pose_stamped_yaw(Point(x=avgpos.x+0.1, y=avgpos.y, z=avgpos.z+0.20), frame_id='head', yaw=avgyaw))
-rospy.sleep(2)
 getavgpos()
-print 'move to pregrab'
+locallib.solve_move_trac(mylimb, locallib.make_pose_stamped_yaw(Point(x=avgpos.x+0.1, y=avgpos.y, z=avgpos.z+0.20), frame_id='head', yaw=avgyaw))
+
+getavgpos()
+print '********* move to pregrab **********'
 locallib.solve_move_trac(mylimb, locallib.make_pose_stamped_yaw(Point(x=avgpos.x, y=avgpos.y, z=avgpos.z+0.12), frame_id='head', yaw=avgyaw))
 
-rospy.sleep(2)
 getavgpos()
-print 'move to grab'
+print '********* move to grab **********'
 locallib.solve_move_trac(mylimb, locallib.make_pose_stamped_yaw(Point(x=avgpos.x, y=avgpos.y, z=avgpos.z+0.04), frame_id='head', yaw=avgyaw))
 
 ###
