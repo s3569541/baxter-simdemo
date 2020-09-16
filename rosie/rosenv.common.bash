50a77882a392f35d063ab13210b0bc5e3816279b
Ian Peake <ian.peake@rmit.edu.au>
2020-09-16 12:22:27 +1000

    refactor incl use of ROS_MASTER_URI

--- rosie/rosenv.common.bash
+++ rosie/rosenv.common.bash
@@ -5,8 +5,18 @@ source ~/rosie/rosenv.local.bash
 
 HOSTNAME=`hostname`
 export ROS_IP=`host ${HOSTNAME} | sed 's/.* //g'`
-export MASTER_IP=`host gazebo | sed 's/.* //g'`
-export ROS_MASTER_URI=http://$MASTER_IP:11311
+
+#MASTERNAME=`echo $ROS_MASTER_URI | sed 's%http://%%' | sed 's%:11311%%'`
+#MASTER_IP=`host ${MASTERNAME} | sed 's/.* //g'`
+#echo HOSTNAME $HOSTNAME 
+#echo MASTERNAME $MASTERNAME
+#if [ "$HOSTNAME" = "$MASTERNAME" ]; then
+#    echo "Is MASTER"
+#    #export ROS_MASTER_URI=http://localhost:11311
+#else
+#    echo "Remote Master"
+#fi
+#echo ROS_MASTER_URI $ROS_MASTER_URI
 
 export MB_LASER_BIRDCAGE_R2000=1
 export MB_LASER_BIRDCAGE_R2000_FREQ=50
