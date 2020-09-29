de66b5b01ddbaf45e05635fd9e82aa03988278dd
Joseph Williams <s3569541@student.rmit.edu.au>
2020-09-29 04:30:26 +0000

    updated scripts to scan tower of blocks w right camera dn replicate with left camera and gripper

--- rosie/moveit/moveit_baxter.py
+++ rosie/moveit/moveit_baxter.py
@@ -59,6 +59,7 @@ global robot
 global mylimb
 mylimb = ''
 global lgripper
+global rgripper
 global display_trajectory_publisher
 
 def init():
@@ -84,100 +85,64 @@ def init():
     lgroup = moveit_commander.MoveGroupCommander("left_arm")
     rgroup = moveit_commander.MoveGroupCommander("right_arm")
 
-    # group.set_max_velocity_scaling_factor(0.07)
-    lgroup.set_max_acceleration_scaling_factor(0.01)
-    rgroup.set_max_acceleration_scaling_factor(0.01)
+    # this is necessaary to reduce number pf joint failures to increase precision of movements
+    lgroup.set_max_acceleration_scaling_factor(0.3)
+    rgroup.set_max_acceleration_scaling_factor(0.3)
 
-    print 'calibrate / open gripper'
+    print 'calibrate / open left gripper'
     global lgripper
-    lgripper = locallib.init_gripper(mylimb)
-    lgripper.calibrate()
-    lgripper.open()
-    rospy.sleep(1)
-    print 'gripper open'
+    lgripper = locallib.init_gripper('left')
 
-    # scene = moveit_commander.PlanningSceneInterface()
-    # rospy.sleep(2)
-    # scene.remove_world_object("table")
-
-    print("~~~~~~init complete~~~~~~~~~~")
+    print("~~~~~~ init and calibration complete ~~~~~~~~~~")
 
 def locateBlock(marker):
-    
-    print "\nLocating marker: ", marker
-    while(locallib.getavgpos('right_hand_camera', marker)==False):
-        rospy.sleep(2)
-
-    avgpos,avgyaw = locallib.getavgpos('right_hand_camera', marker)
-
-    print "New Pos: ", avgpos, "\nYaw: ",avgyaw
-    # mylimb =  'right'
-    # newPoseBase = locallib.translate_frame(locallib.make_pose_stamped_yaw(avgpos, 'head', avgyaw), 'base')
-    # pos = newPoseBase.pose.position
-    # ori = quaternion_from_euler(3.14, 0, avgyaw)
-
-    # print 'Scanning......'
-    # move_arm(mylimb, pos.x, pos.y+0.05, pos.z+0.08, 0.521617116774, 0.474482274666, -0.513181385153, 0.489312804297)
-
-    # print("Updating marker: ", marker, " position...", )
-    # avgpos,avgyaw = locallib.getavgpos('right_hand_camera', marker)
-    # newPoseBase = locallib.translate_frame(locallib.make_pose_stamped_yaw(avgpos, 'head', avgyaw), 'base')
-    # pos = newPoseBase.pose.position
-    # ori = quaternion_from_euler(3.14, 0, avgyaw)
-    # move_arm(mylimb, pos.x, pos.y+0.06, pos.z+0.05, 0.521617116774, 0.474482274666, -0.513181385153, 0.489312804297)
-    # print("Updating marker: ", marker, " position...")
-    # avgpos,avgyaw = locallib.getavgpos('right_hand_camera', marker)
+    print "\nLocating block: ", marker
+    avgpos,avgyaw = locallib.getavgpos(marker)
 
     return avgpos,avgyaw
 
 def pick(target_marker_id):
 
-    # Move to idea scan position
-    # move_arm( mylimb, 0.6, 0.7, -0.1)
     move_arm('left', 0.65, 0.7, -0.1)
+    print "Moving to inital scan position"
 
     global lgripper
     global mylimb
 
-    while(not locallib.getavgpos('left_hand_camera', target_marker_id)):
-        rospy.sleep(1)
-
-    avgpos, avgyaw = locallib.getavgpos('left_hand_camera', target_marker_id)
+    avgpos, avgyaw = locallib.getavgpos(target_marker_id)
 
     print '******* move to proper sighting pos ********'
     newPoseBase = locallib.translate_frame(locallib.make_pose_stamped_yaw(avgpos, 'head', avgyaw), 'base')
     pos = newPoseBase.pose.position
     ori = quaternion_from_euler(3.14, 0, avgyaw)
 
-    move_arm(mylimb, pos.x, pos.y, pos.z+0.12)#, ori[0], ori[1], ori[2], ori[3])
+    move_arm(mylimb, pos.x, pos.y, pos.z+0.1)#, ori[0], ori[1], ori[2], ori[3])
 
-    # terminate()
-
-    rospy.sleep(2)
-    avgpos,avgyaw = locallib.getavgpos('left_hand_camera', target_marker_id)
+    print "Updating block", target_marker_id, "location..."
+    rospy.sleep(0.5)
+    avgpos,avgyaw = locallib.getavgpos(target_marker_id)
 
     newPoseBase = locallib.translate_frame(locallib.make_pose_stamped_yaw(avgpos, 'head', avgyaw), 'base')
     pos = newPoseBase.pose.position
     ori = quaternion_from_euler(3.14, 0, avgyaw)
 
-    move_arm(mylimb, pos.x, pos.y, pos.z+0.07)#, ori[0], ori[1], ori[2], ori[3])
-    # drop_arm(mylimb, pos.x, pos.y, pos.z+0.05)
+    # move_arm(mylimb, pos.x, pos.y, pos.z+0.07)#, ori[0], ori[1], ori[2], ori[3])
+    drop_arm(mylimb, pos.x, pos.y, pos.z+0.07)
 
-    # terminate()
-
-    rospy.sleep(2)
-    avgpos,avgyaw = locallib.getavgpos('left_hand_camera', target_marker_id)
+    print "Updating block", target_marker_id, "location..."
+    rospy.sleep(0.5)
+    avgpos,avgyaw = locallib.getavgpos(target_marker_id)
 
     print 'move to grab'
     newPoseBase = locallib.translate_frame(locallib.make_pose_stamped_yaw(avgpos, 'head', avgyaw), 'base')
     pos = newPoseBase.pose.position
     ori = quaternion_from_euler(3.14, 0, avgyaw)
 
-    drop_arm(mylimb, pos.x, pos.y, pos.z+0.02)
+    drop_arm(mylimb, pos.x, pos.y, pos.z+0.01)
 
     ### Close grippers
     print 'close (grab)'
-    rospy.sleep(1.0)
+    rospy.sleep(0.5)
     lgripper.close()
     print 'lift'
     raise_arm(0.1)
@@ -191,11 +156,11 @@ def place(marker, avgpos, avgyaw):
     ori = quaternion_from_euler(3.14, 0, avgyaw)
 
     print 'Stack'
-    move_arm(mylimb, pos.x, pos.y*-1, pos.z+0.04)#, ori[0], ori[1], ori[2], ori[3])
+    move_arm(mylimb, pos.x, pos.y*-1, pos.z+0.04)#, ori[0]), ori[1], ori[2], ori[3])
 
-    rospy.sleep(1.0)
+    rospy.sleep(0.5)
     lgripper.open()
-    rospy.sleep(1.0)
+    rospy.sleep(0.5)
 
     raise_arm(0.1)
 
@@ -205,9 +170,6 @@ def move_arm(limb, x, y, z, ox = 0.0, oy = 1.0, oz = 0.0, ow = 0.0):
     global robot
     global display_trajectory_publisher
     # Planning to a Pose goal
-    # print "\n\nLeft gripper current state"
-    # print rgroup.get_current_pose(end_effector_link='right_gripper')
-    # terminate()
     
     if(limb == 'left'):
         left_current_pose = lgroup.get_current_pose(end_effector_link='left_gripper').pose
@@ -223,10 +185,8 @@ def move_arm(limb, x, y, z, ox = 0.0, oy = 1.0, oz = 0.0, ow = 0.0):
         plan = lgroup.plan()
         if not plan.joint_trajectory.points:
             print "[ERROR] No trajectory found"
-            # terminate()
         else:
             lgroup.go(wait=True)
-            rospy.sleep(0.5)
 
     if(limb == 'right'):
         right_current_pose = rgroup.get_current_pose(end_effector_link='right_gripper').pose
@@ -242,16 +202,8 @@ def move_arm(limb, x, y, z, ox = 0.0, oy = 1.0, oz = 0.0, ow = 0.0):
         plan = rgroup.plan()
         if not plan.joint_trajectory.points:
             print "[ERROR] No trajectory found"
-            # terminate()
         else:
             rgroup.go(wait=True)
-            rospy.sleep(0.5)
-
-    # display_trajectory = moveit_msgs.msg.DisplayTrajectory()
-    # display_trajectory.trajectory_start = robot.get_current_state()
-    # display_trajectory.trajectory.append(plan)
-    # # Publish
-    # display_trajectory_publisher.publish(display_trajectory);
 
 def drop_arm(mylimb, x, y, z):
     # adapted from: 
@@ -295,7 +247,7 @@ def drop_arm(mylimb, x, y, z):
     group.execute(plan, wait=True)
 
 def raise_arm(height):
-    # adapted from: 
+   # adapted from: 
     # https://ros-planning.github.io/moveit_tutorials/doc/move_group_python_interface/move_group_python_interface_tutorial.html#getting-started
     global lgroup
     waypoints = []
@@ -320,83 +272,6 @@ def raise_arm(height):
     # return plan, fraction 
 
     lgroup.execute(plan, wait=True)
-
-def update_scene(blocks):
-    ## Instantiate a `PlanningSceneInterface`_ object.  This object is an interface
-    ## to the world surrounding the robot:
-    ## Needs to be constructed after robot and group, or "can't get joint states"
-    scene = moveit_commander.PlanningSceneInterface()
-    # Wait on group/scene?
-    # FIXME: is there a proper way to do this
-    rospy.sleep(2)
-    scene.remove_world_object("table")
-    scene.remove_world_object("box_1")
-    scene.remove_world_object("box_2")
-    scene.remove_world_object("box_3")
-
-    #adding planning scene objects, initially to avoid collisions has resulted in error and issues and complexity in motion planning, 
-    # seems to work better if you simply set way points to aid planning and collision avoidance
-
-    ## Instantiate a `PlanningSceneInterface`_ object.  This object is an interface
-    ## to the world surrounding the robot:
-    ## Needs to be constructed after robot and group, or "can't get joint states"
-    # scene = moveit_commander.PlanningSceneInterface()
-    # # Wait on group/scene?
-    # # FIXME: is there a proper way to do this
-    # rospy.sleep(2)
-    
-    # scene.remove_world_object("table")
-    # table = geometry_msgs.msg.PoseStamped()   
-    # table.header.frame_id = "head"
-    # table.pose.position.x =  1.08
-    # table.pose.position.y = -0.00919116929443
-    # table.pose.position.z = -1.3
-    # table.pose.orientation.w = 1.0
-    # box_name = "table"
-    # scene.add_box(box_name, table, size=(1.5, 0.8, 0.82))
-    # wait_for_state_update(obj_name='table',box_is_known=True)
-
-    # rospy.sleep(2) 
-
-    for block in blocks:
-        newPoseBase = locallib.translate_frame(locallib.make_pose_stamped(blocks[block]['AvgPos'], 'head', locallib.return_yaw(blocks[block]['avgyaw'])), 'base')
-        pos = newPoseBase.pose.position
-        ori = newPoseBase.pose.orientation
-        box_name = "box_"+str(block)
-        box = geometry_msgs.msg.PoseStamped()   
-        box.header.frame_id = "base"
-        box.pose.position.x = pos.x
-        box.pose.position.y = pos.y
-        box.pose.position.z = pos.z
-        box.pose.orientation.w = 1.0
-        scene.add_box(box_name, box, size=(0.04, 0.04, 0.04))
-        wait_for_state_update(obj_name=box_name,box_is_known=True)
-    
-def delete_scene():
-    scene = moveit_commander.PlanningSceneInterface()
-    scene.remove_world_object("box_1")
-    scene.remove_world_object("box_2")
-    scene.remove_world_object("box_3")
-
-def wait_for_state_update(box_is_known=False, box_is_attached=False, timeout=4, obj_name='box'):
-    scene = moveit_commander.PlanningSceneInterface()
-    start = rospy.get_time()
-    seconds = rospy.get_time()
-    while (seconds - start < timeout) and not rospy.is_shutdown():
-      # Test if the box is in attached objects
-      attached_objects = scene.get_attached_objects([obj_name])
-      is_attached = len(attached_objects.keys()) > 0
-      # Test if the box is in the scene.
-      # Note that attaching the box will remove it from known_objects
-      is_known = obj_name in scene.get_known_object_names()
-      # Test if we are in the expected state
-      if (box_is_attached == is_attached) and (box_is_known == is_known):
-        return True
-      # Sleep so that we give other threads time on the processor
-      rospy.sleep(0.1)
-      seconds = rospy.get_time()
-    # If we exited the while loop without returning then we timed out
-    return False
 
 def terminate():
     global lgroup
--- rosie/moveit/pickup_alvar_demo.py
+++ rosie/moveit/pickup_alvar_demo.py
@@ -7,6 +7,7 @@ import Vectors
 import struct
 import threading
 import Queue
+import copy 
 import rospy
 from std_msgs.msg import String
 import sys
@@ -22,59 +23,56 @@ global pos_in_frame
 global topbot
 global center_pose
 
-initial_pose = {} # to be populated from left hand camera
-goal_pose = {} # to be populated from right hand camera
-
-
-
 locallib.init(nodename='pickup_alvar_demo')
 moveit_baxter.init()
 
-# locallib.init()
+availableMarkers = [0,10,20,30]
+
+'''
+the following will put the right arm in a good position for pyramid stack
+this needs to be run on initial start up
+'''
+# print("Move arm to initial scanning position")
+# moveit_baxter.move_arm('left', 0.55, 0.7, -0.1)
+# rgripper = locallib.init_gripper("right")
+# moveit_baxter.move_arm('right', 0.57, -0.55, -0.09, 0.475801111046, 0.520671447619, -0.488470343501, 0.513722950511)
+# moveit_baxter.terminate()
 
 global mylimb
 mylimb = 'left'
 
 markers = {}
-# the following will put the right arm in a good position for pyramid stack
-# this needs to be run on initial set up
-# print("Move arm to initial scanning position")
-# rgripper = locallib.init_gripper("right")
-# rgripper.calibrate()
-# rgripper.open()
-# moveit_baxter.move_arm('right', 0.57, -0.55, -0.12, 0.475801111046, 0.520671447619, -0.488470343501, 0.513722950511)
-
 while markers == {}:
-    rospy.sleep(2)
-    markers = locallib.avgmarkerpos
+    rospy.sleep(0.5)
+    markers = copy.deepcopy(locallib.avgmarkerpos)
 
-print "\nDetected Block Numbers"
+print "\nKnown Block Numbers"
 for marker in markers:
   print marker
+
+rightStackMarkers = {}
 # Get locations of all blocks in right arm stack
 for marker in markers:
-  # occasionally the alvar systems thinks it sees 255 when it doesnt exist
-  if marker != 255:
-    moveit_baxter.locateBlock(marker)
+  if marker in availableMarkers:
+    avgpos,avgyaw = moveit_baxter.locateBlock(marker)
+    if avgpos != 0:
+      rightStackMarkers[marker] = {"AvgPos": avgpos, "avgyaw" : avgyaw}
+
+for marker in rightStackMarkers:
+  print marker
+  print rightStackMarkers[marker], "\n"
 
 # sort the right stack block ids by z axis value so the lowest blocks get stacked first
-rightstack = locallib.getRightBlockPositions()
-
 stackOrder = []
-for marker in rightstack:
-  if marker != 255:
-    stackOrder.append((rightstack[marker]['AvgPos'].z, marker))
+for marker in rightStackMarkers:
+  stackOrder.append((rightStackMarkers[marker]['AvgPos'].z, marker))
 
 def sortSecond(val): 
   return val[0]
 stackOrder.sort(key=sortSecond) 
 
 for marker in stackOrder:
-  # occasionally the alvar systems thinks it sees 255 when it doesnt exist
-  if marker[1] != 255:
-    moveit_baxter.pick(marker[1])
-    moveit_baxter.place(marker[1], locallib.getRightBlockPositions()[marker[1]]['AvgPos'], locallib.getRightBlockPositions()[marker[1]]['avgyaw'])
-
-moveit_baxter.move_arm(mylimb, 0.6, 0.0, 0.1)
+  moveit_baxter.pick(marker[1])
+  moveit_baxter.place(marker[1], rightStackMarkers[marker[1]]['AvgPos'], rightStackMarkers[marker[1]]['avgyaw'])
 
 moveit_baxter.terminate()
